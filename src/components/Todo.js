575abadcfd4f9233a44f10ce7dd602476cdb607b
mansurhamidov14 <mansur.hamidoff.1994@gmail.com>
2018-06-01 04:14:59 +0400

    Fixed width for control column and z-index for header

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Todo = ({todoId, text, onToggleClick, onDeleteClick, completed}) => (
   <tr className={completed ? 'table-success' : 'table-warning'}>
     <td>{text}</td>
-    <td className="text-right">
+    <td className="text-right px-w-120">
       <button
         className={completed ? 'btn btn-success fa fa-check-square-o mr-2' : 'btn btn-warning fa fa-warning mr-2'}
         type="button"
