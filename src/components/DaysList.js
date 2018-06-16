a9aee985b84a179068e4d4c887239aefc7e9d879
mansurhamidov14 <mansur.hamidoff.1994@gmail.com>
2018-06-16 09:40:16 +0400

    Setting today weekday active in weekday list

--- src/components/DaysList.js
+++ src/components/DaysList.js
@@ -4,12 +4,13 @@ import { connect } from 'react-redux';
 import { setPageToView, setShownDate } from '../actions';
 import strings from '../translates/strings';
 
+const currentDay = new Date().getDay();
 const DaysList = ({currentDate, setPageToView, setShownDate, language}) => (
   <div className="col-12 mt-3">
     <div className="list-group" id="list-tab" role="tablist">
       {[1, 2, 3, 4, 5, 6, 0].map(i =>
         <Link key={i}
-          className={i === currentDate ? "list-group-item list-group-item-action active" : "list-group-item list-group-item-action"}
+          className={i === currentDay ? "list-group-item list-group-item-action active" : "list-group-item list-group-item-action"}
           to="/home"
           onClick={() => {setPageToView('/home');setShownDate(i)}}>
           {strings[language]['week_days'][i]}
